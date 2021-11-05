43543e58f390312b8aeb936844a26fd402d86f78
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-05 16:02:53 +0300

    Update docker_scan.sh

--- tool/docker_scan.sh
+++ tool/docker_scan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker scan --login --token da950e95-9d26-488c-9a83-3b848751af83
-docker scan --accept-license --json devslavus/myschool:latest > coverage/dreport.json
+docker scan --accept-license --json devslavus/myschool:latest > coverage/dreport.html
 sleep 10
