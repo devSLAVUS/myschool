7cc7c48538e98a7fe700e1bff21ff7e855151720
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-05 12:49:14 +0300

    Update djangotest.sh

--- tool/djangotest.sh
+++ tool/djangotest.sh
@@ -3,6 +3,16 @@ cd ..
 cd django_school/
 pwd
 ls
+name="python3-venv"
+I=`dpkg -s $name | grep "Status" `
+if [ -n "$I" ]
+then
+   echo $name" installed"
+else
+   echo $name" not installed"
+   apt update
+   apt-get install python3-venv
+fi
 python3 -m venv env
 source ./env/bin/activate
 pip install -r requirements.txt
