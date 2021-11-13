46756036e4f0649f73a102e55458f1f7ef819d10
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-13 18:33:43 +0300

    Update drun.sh

--- tool/drun.sh
+++ tool/drun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+source /etc/environment.d/jenenv.conf
 ssh -t root@prodstage "hostname; docker pull $DOCKER_HUB_REPO/$IMAGE_NAME; docker run --name $IMAGE_NAME -d -p 80:80 $DOCKER_HUB_REPO/$IMAGE_NAME:latest"
 sleep 5
