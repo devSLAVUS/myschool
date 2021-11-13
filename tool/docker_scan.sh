3cac5599792de43c066c652a2a72d32b716636ab
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-13 16:32:53 +0300

    Update docker_scan.sh

--- tool/docker_scan.sh
+++ tool/docker_scan.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-source ~/load_env.sh
-echo $DTOKEN
+
 docker scan --login --token $DTOKEN
-docker scan --accept-license devslavus/myschool:latest > coverage/dreport.txt
+docker scan --accept-license $DOCKER_HUB_REPO/$IMAGE_NAME:latest > coverage/dreport.txt
 sleep 10
 txt2html coverage/dreport.txt > coverage/dreport.html
