2859f5a02055e23c0e6c2eebdf43201f9649bb40
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-14 16:18:30 +0300

    Update docker_scan.sh

--- tool/docker_scan.sh
+++ tool/docker_scan.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /etc/environment.d/jenenv.conf
 docker scan --login --token $DTOKEN
 docker scan --accept-license $DOCKER_HUB_REPO/$IMAGE_NAME:latest > coverage/dreport.txt
 sleep 10
